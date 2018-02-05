4871e921e4616130b18e30b39dbd64379b94bc57
nanocloudx <nanocloudx@gmail.com>
2018-02-06 04:09:26 +0900

    initialize

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,58 @@ export default {
 </script>
 
 <style>
+html {
+  background-color: #555555;
+}
+body {
+  background: #fafafa;
+  margin: 0;
+}
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "Hiragino Kaku Gothic ProN", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #444444;
+}
+.container {
+  margin: 0 auto;
+  padding: 0 10px;
+  @media screen and (min-width: 635px) {
+    max-width: 600px;
+  }
+  @media screen and (min-width: 835px) {
+    max-width: 800px;
+  }
+  @media screen and (min-width: 1035px) {
+    max-width: 1000px;
+  }
+}
+h2 {
+  font-size: 32px;
+  padding: 20px 0;
+}
+h3 {
+  font-size: 24px;
+  padding: 20px 0;
+}
+button {
+  margin: 20px;
+  padding: 10px 40px;
+  font-size: 16px;
+  border: none;
+  border-radius: 3px;
+  background-color: #00aced;
+  color: #ffffff;
+  cursor: pointer;
+}
+li {
+  list-style: none;
+}
+.center {
+  margin: 0 auto;
+  text-align: center;
+}
+.strike {
+  text-decoration: line-through;
 }
 </style>
--- src/components/Sleep.vue
+++ src/components/Sleep.vue
@@ -1,15 +1,92 @@
 <template>
-  <div>
-    <img src="../assets/building_bigsight.png">
-    <h1>にゃんぱす！</h1>
+  <div class="index">
+    <div class="welcome">
+      <div class="message">
+        <h1>Twitterサークル検索サービス</h1>
+        <h1><span class="title">「にゃんぱす！」</span></h1>
+        <img src="../assets/building_bigsight.png">
+      </div>
+      <div class="cutter"></div>
+    </div>
+
+    <div class="info container center">
+      <div class="section">
+        <h2>おやすみ中</h2>
+        <p>にゃんぱす！をご利用いただきありがとうございます。</p>
+        <p>本サービスはコミックマーケット開催期間前後に役立つ機能を提供しています。</p>
+        <p>現在は<span class="strike">サーバー費用が苦しい</span>閑散期の為、サービスの機能を一時的に停止しております。</p>
+        <p>ご不便をおかけいたしますが、サービス再開までしばらくお待ち下さい。</p>
+        <p>※現在、再開時期は未定です(2017/02/01更新)</p>
+      </div>
+      <div class="section">
+        <p><a href="https://www.kurokuroworks.net/">サークルページはこちら</a></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Sleep'
-}
+  export default {
+    name: 'Sleep'
+  }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .index {
+    padding-bottom: 100px;
+  }
+  .welcome {
+    background-image: url("../assets/welcome.jpg");
+    background-size: cover;
+    background-position: center;
+    overflow: hidden;
+    .message {
+      text-align: center;
+      margin: 30px 0 60px;
+      height: 400px;
+      img {
+        height: 140%;
+        overflow: hidden;
+        filter: drop-shadow(0 0 20px rgba(0,0,0,0.5));
+      }
+      h1 {
+        font-size: 30px;
+        padding: 5px;
+        color: #ffffff;
+        .title {
+          font-size: 35px;
+        }
+      }
+    }
+  }
+  .section {
+    margin-bottom: 20px;
+  }
+  .info {
+    h2 {
+      margin: 0;
+      font-size: 1.6rem;
+    }
+    p {
+      margin: 0;
+    }
+  }
+  .welcome .cutter {
+    position: relative;
+    background-color: #555;
+    width: 120%;
+    height: 200px;
+    margin: 0 -10% -150px;
+    transform: rotate(3deg);
+  }
+  .welcome .cutter:before {
+    content: "";
+    position: absolute;
+    width: 120%;
+    height: 400px;
+    margin: 0 -10%;
+    background: #fafafa;
+    transform: rotate(-6deg);
+  }
+
 </style>
