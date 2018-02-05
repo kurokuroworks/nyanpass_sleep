bd8890d58c3e636c8bf1d5738c8a2e1c832d68c1
nanocloudx <nanocloudx@gmail.com>
2018-02-06 03:47:29 +0900

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sleep />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sleep from './components/Sleep.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sleep
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Sleep.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="../assets/building_bigsight.png">
+    <h1>にゃんぱす！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sleep'
+}
+</script>
+
+<style scoped>
+</style>
